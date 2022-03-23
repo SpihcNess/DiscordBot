413c07b1a543f226538a97c7c4b6a199f86d90b8
Mathis <mathis.langrand@gmail.com>
2022-03-23 17:52:09 +0100

    prout

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,15 +1,33 @@
-import discord.py
+import discord
+from discord.ext import commands as cmd
 
-class DiscordBot(object):
-    name = ""
-    client = discord.client
-    # Setters
+# CREATION DU TOKEN
+token = "OTU2MjA3NTA1MDQ0ODMyMzI2.Yjs3og.KQuny46KaulepOo82AQcXobh3yg"
+# CREATION DU CLIENT
+client = discord.Client()
+# DEFINITION DU PREFIXE DE CHAQUE COMMANDE
+bot = cmd.Bot(command_prefix = "!")
 
-    # Getters
+# MESSAGE WHEN LE BOT IS EN LIGNE
+@client.event
+async def on_ready():
+    print("client en ligne")
+@bot.event
+async def on_ready():
+    print("bot en ligne")
 
+# MESSAGE WHEN QQUN MET UN MESSAGE PARTICULIER
+#@bot.event
+#async def on_message(message):
+#    if (message.content.lower() == "ping"):
+#        await message.channel.send("ta gueule")
+#    elif (message.content.lower() == "mais..."):
+#        await message.channel.send("**TA GUEULE**")
 
-    # Constructor
-    #def __init__(name):
+@bot.command(name = "del")
+async def delete(ctx, number: int):
+    messages = await ctx.channel.history(limit = number + 1).flatten()
+    for message in messages:
+        await message.delete()
 
-
-    # Functions
+bot.run(token)
